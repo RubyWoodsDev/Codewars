2ee65c416cec91e9e5017f0d67328904aa93572a
Ruthie 'Ruby K' Woods <woods.ruthie@gmail.com>
2021-12-31 13:15:04 -0500

    easy logs 8kyu

---
+++ easy-logs/main.js
@@ -0,0 +1,6 @@
+//DESCRIPTION:
+//Add two logs with base X, with the value of A and B. Example log A + log B where the base is X.
+solution
+function logs(x , a, b){
+    return (Math.log(a*b) / Math.log(x));
+    }
