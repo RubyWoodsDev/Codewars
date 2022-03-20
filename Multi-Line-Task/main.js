1a03a3fd7001e1e438309b53767fdd1de88e717d
Ruthie 'Ruby K' Woods <woods.ruthie@gmail.com>
2022-03-20 10:27:42 -0500

    Multi Line Task++: Hello World-2kyu

---
+++ Multi-Line-Task/main.js
@@ -0,0 +1,41 @@
+//DESCRIPTION:
+//You need to write a function f that returns the string Hello, world!.
+
+//Requirement: Every line must have at most 2 characters, and total number of lines must be less than 40.
+
+//Hint: It's possible to complete this in 28 lines only.
+
+
+
+
+
+solution
+
+f=
+''
+[
+'\
+t\
+r\
+i\
+m'
+][
+'\
+b\
+i\
+n\
+d'
+]`
+H\
+e\
+l\
+l\
+o\
+,\
+ \
+w\
+o\
+r\
+l\
+d\
+!`
