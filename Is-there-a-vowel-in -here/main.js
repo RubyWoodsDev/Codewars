621ade26dc73c40d2018a0a641097b279fe1a38f
Ruthie 'Ruby K' Woods <woods.ruthie@gmail.com>
2022-03-21 16:32:09 -0500

    is here a vowel in there-8kyu

---
+++ Is-there-a-vowel-in -here/main.js
@@ -0,0 +1,22 @@
+//DESCRIPTION:
+//Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
+
+//If they are, change the array value to a string of that vowel.
+
+//Return the resulting array.
+
+
+solution
+function isVow(a){
+
+}
+function isVow(a){
+  for (var i=0, l=a.length; i<l; ++i)
+  {
+    var char = String.fromCharCode(a[i])
+    if ('aeiou'.indexOf(char) !== -1)
+    a[i] = char;
+  }
+  
+  return a;
+}
