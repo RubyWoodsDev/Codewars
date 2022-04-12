558b92fbf0c09631fc6119278d460e855c8c4f67
Ruthie 'Ruby K' Woods <woods.ruthie@gmail.com>
2022-04-12 16:18:48 -0500

    beginner reduce but grow -8kyu

---
+++ Beginner-Reduce-but-grow/main.js
@@ -0,0 +1,10 @@
+//DESCRIPTION:
+//Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+//[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+solution
+function grow(x){
+    return x.reduce((prod,v) => prod * v);
+  
+  }
