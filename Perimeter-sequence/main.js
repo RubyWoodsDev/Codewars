f196b25a3038d8dcccdf2790070c5a609ab81932
Ruthie 'Ruby K' Woods <woods.ruthie@gmail.com>
2022-05-25 21:56:03 -0500

    perimeter sequence -8kyu

---
+++ Perimeter-sequence/main.js
@@ -0,0 +1,15 @@
+//DESCRIPTION:
+//Perimeter sequence
+//The first three stages of a sequence are shown.
+
+//blocks
+
+//The blocksize is a by a and a ≥ 1.
+
+//What is the perimeter of the nth shape in the sequence (n ≥ 1) ?
+
+
+solution
+{
+    perimeterSequence = (a, n)  => a * n * 4
+    }
