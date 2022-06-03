a707f22aed198cf72bf23f181567d835282e0568
Ruthie 'Ruby K' Woods <woods.ruthie@gmail.com>
2022-06-03 15:05:39 -0500

    FIXME: Replace all dots 8kyu

---
+++ FIXME/main.js
@@ -0,0 +1,16 @@
+//DESCRIPTION:
+//The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+//But it's not working properly.
+
+//Task
+//Fix the bug so we can all go home early.
+
+//Notes
+//String str will never be null.
+
+
+solution
+var replaceDots = function(str) {
+    return str.replace(/\./g,'-');
+  }
