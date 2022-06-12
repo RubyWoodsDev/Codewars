0d45b4d964aff5bf8b075d341d93864ca5c7f380
Ruthie 'Ruby K' Woods <woods.ruthie@gmail.com>
2022-06-12 16:26:32 -0500

    Third Angle of a Triangle 8 kyu

---
+++ Third-Angle-of-a-Triangle/main.js
@@ -0,0 +1,15 @@
+//DESCRIPTION:
+//You are given two interior angles (in degrees) of a triangle.
+
+//Write a function to return the 3rd.
+
+//Note: only positive integers will be tested.
+
+//https://en.wikipedia.org/wiki/Triangle
+
+//FUNDAMENTALS
+
+solution
+function otherAngle(a, b) {
+    return 180-(a+b);
+  }
