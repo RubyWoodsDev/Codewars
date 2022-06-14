71d94eb53125c0886203cb34851e266d4ca87389
Ruthie 'Ruby K' Woods <woods.ruthie@gmail.com>
2022-06-14 17:05:49 -0500

    updated comment -sum the strings 8kyu

--- Sum-The-Strings/main.js
+++ Sum-The-Strings/main.js
@@ -13,7 +13,7 @@
 //If either input is an empty string, consider it as zero.
 
 //Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
-
+//update
 
 solution
 function sumStr(a,b) {
