ee760f13fb6d6cd952fa3c4b5ce01351bfc05136
Ruthie 'Ruby K' Woods <woods.ruthie@gmail.com>
2022-06-25 19:32:41 -0500

    find nearest square number 8 kyu

---
+++ Find-Nearest-square/main.js
@@ -0,0 +1,8 @@
+//DESCRIPTION:
+//Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+
+//Goodluck :)
+
+
+solution
+const nearestSq = n => Math.pow(Math.round(Math.sqrt(n)), 2);
