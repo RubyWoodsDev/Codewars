728bd67f8579193f3d953c6f10da0b49b00ca840
Ruthie 'Ruby K' Woods <woods.ruthie@gmail.com>
2022-07-04 15:32:38 -0500

    No Loops 2 - You only need one 8kyu

---
+++ no-loops/main.js
@@ -0,0 +1,11 @@
+//DESCRIPTION:
+//*** No Loops Allowed ***
+
+//You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value, without using a loop.
+
+//Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+
+solution
+function check(a,b){
+    return a.includes(b);
+  };
