7e3c0468afaa52105aabe1608a4b193fd402349e
Ruthie 'Ruby K' Woods <woods.ruthie@gmail.com>
2022-07-09 16:44:48 -0500

    Switch it Up 8kyu

---
+++ switch-it-up/main.js
@@ -0,0 +1,11 @@
+//DESCRIPTION:
+//When provided with a number between 0-9, return it in words.
+
+//Input :: 1
+
+//Output :: "One".
+
+//If your language supports it, try using a switch statement.
+
+solution
+switchItUp=n=>["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][n]
