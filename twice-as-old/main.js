6fbe107f6406b56a9ead938bfd07bd944652ca0f
Ruthie 'Ruby K' Woods <woods.ruthie@gmail.com>
2022-07-21 13:38:48 -0400

    added an update to comments

--- twice-as-old/main.js
+++ twice-as-old/main.js
@@ -4,6 +4,7 @@
 //current father's age (years)
 //current age of his son (years)
 //Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+//update
 solution
 function twiceAsOld(dadYearsOld, sonYearsOld) {
     return Math.abs(dadYearsOld - 2 * sonYearsOld);
