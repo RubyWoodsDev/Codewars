a7207965333e09f104e6317df0a45c6375df13fd
Ruthie 'Ruby K' Woods <woods.ruthie@gmail.com>
2022-08-08 17:05:47 -0400

    added update to comments

---
+++ get- character/main.js
@@ -0,0 +1,17 @@
+//DESCRIPTION:
+//Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+//Example:
+
+//get_char(65)
+//should return:
+
+//'A'
+//For ASCII table, you can refer to http://www.asciitable.com/
+
+//updated
+
+solution
+function getChar(c){
+    return String.fromCharCode(c);
+    }
